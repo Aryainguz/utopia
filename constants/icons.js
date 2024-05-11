821b457dc21e4746b0fa5706ead93834bead0129
Aryainguz <aryaninguz369@gmail.com>
2024-05-11 19:08:09 +0530

    feat: Add icons.js and index.js files for constants

---
+++ constants/icons.js
@@ -0,0 +1,40 @@
+import bookmark from "../assets/icons/bookmark.png";
+import bookmark2 from "../assets/icons/bookmark2.png";
+import home from "../assets/icons/home.png";
+import plus from "../assets/icons/plus.png";
+import profile from "../assets/icons/profile.png";
+import leftArrow from "../assets/icons/left-arrow.png";
+import menu from "../assets/icons/menu.png";
+import search from "../assets/icons/search.png";
+import upload from "../assets/icons/upload.png";
+import rightArrow from "../assets/icons/right-arrow.png";
+import logout from "../assets/icons/logout.png";
+import eyeHide from "../assets/icons/eye-hide.png";
+import eye from "../assets/icons/eye.png";
+import play from "../assets/icons/play.png";
+import create2 from "../assets/icons/create2.png";
+import home2 from "../assets/icons/home2.png";
+import trending from "../assets/icons/trending.png";
+import profile2 from "../assets/icons/profile2.png";
+
+
+export default {
+  play,
+  bookmark,
+  bookmark2,
+  home,
+  plus,
+  profile,
+  leftArrow,
+  menu,
+  search,
+  upload,
+  rightArrow,
+  logout,
+  eyeHide,
+  eye,
+  create2,
+  home2,
+  trending,
+  profile2
+};
