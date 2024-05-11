e7ba0907c8eab1fc1ffb4456f8573a949588aa2e
Aryainguz <aryaninguz369@gmail.com>
2024-05-11 19:06:19 +0530

    chore: Add RootLayout component and Index page

---
+++ app/index.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { Image, ImageBackground, ScrollView, Text, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import illustration from "../assets/images/Saly-17.png";
+import { Link } from "expo-router";
+import logo from "../assets/images/triangular-logo.png";
+
+
+
+const Index = () => {
+
+  return (
+    <ImageBackground source={require('../assets/images/screen.png')} style={{flex: 1}}>
+ <SafeAreaView className="h-full">
+  <ScrollView  contentContainerStyle={{height:"100%"}}>
+    <View className="w-full justify-center items-center h-full px-4">
+      <View className="mb-28">
+        <View className="flex justify-center items-center bg-purple-500 h-14 w-14 mb-2 rounded-full">
+          <Image source={logo} className="h-7 w-7" />
+        </View>
+      <Text className="font-pregular text-2xl text-white">
+Express yourself in 
+      </Text>
+<Text className="font-pbold text-7xl text-white">
+        Utopia.
+      </Text>
+      <TouchableOpacity className="bg-violet-400 rounded-xl p-4 my-2 w-[70vw]">
+        <Link href={'/timeline'}>
+        <Text className="text-white font-pregular text-center text-base"> 
+          Enter Now
+        </Text>
+        </Link>
+      </TouchableOpacity>
+      </View>
+    <Image
+     source={illustration}
+     className="absolute h-72 w-full bottom-[0px]"
+    /> 
+    </View>
+  </ScrollView>
+  
+ </SafeAreaView>
+</ImageBackground>
+
+  );
+};
+
+
+export default Index;
