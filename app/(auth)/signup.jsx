87d046e4b2a6fc121799a8792408b8d9e6a69058
Aryainguz <aryaninguz369@gmail.com>
2024-05-15 09:06:56 +0530

    feat: Add SectionedMultiSelect component for selecting clans in SignUp

--- app/(auth)/signup.jsx
+++ app/(auth)/signup.jsx
@@ -5,6 +5,7 @@ import {
   Dimensions,
   Image,
   ScrollView,
+  StyleSheet,
   Text,
   TouchableOpacity,
   View,
@@ -12,14 +13,33 @@ import {
 import { SafeAreaView } from "react-native-safe-area-context";
 import logo from "../../assets/images/triangular-logo.png";
 import FormField from "../../components/FormField";
+import SectionedMultiSelect from "react-native-sectioned-multi-select";
+import { MaterialIcons as Icon } from "@expo/vector-icons";
 
-const SignIn = () => {
+const items = [
+  { name: "Software", id: 1 },
+  { name: "Sports", id: 2 },
+  { name: "Booknotics", id: 3 },
+  { name: "Enterntainment", id: 4 },
+  { name: "Politics and News", id: 5 },
+];
+
+const SignUp = () => {
+  const [selectedItems, setSelectedItems] = useState([]);
+
   const [form, setForm] = useState({
-    email: "",
+    username: "",
     password: "",
   });
 
   const submit = async () => {};
+
+  const changeSelectedItems = (selectedItems) => {
+    if (selectedItems.length > 2) {
+      selectedItems.shift();
+    }
+    setSelectedItems(selectedItems);
+  };
 
   return (
     <SafeAreaView className="bg-primary h-full">
@@ -35,31 +55,50 @@ const SignIn = () => {
           </View>
 
           <Text className="text-2xl font-semibold text-white mt-6 font-psemibold">
-            Log in to utopia.
+            SignUp to utopia.
           </Text>
 
           <FormField
-            title="Email"
-            value={form.email}
-            handleChangeText={(e) => setForm({ ...form, email: e })}
+            title="Username"
+            value={form.username}
+            handleChangeText={(e) => setForm({ ...form, username: e })}
             otherStyles="mt-7"
-            keyboardType="email-address"
+            keyboardType="username-address"
           />
+
+          <View className="space-y-2 mt-2">
+            <Text className="text-base text-gray-100 font-pmedium py-2">
+              Select Clans
+            </Text>
+            <View className="w-full h-16 bg-black-100 rounded-2xl my-7 border-black-200 focus:border-violet-400">
+              <SectionedMultiSelect
+                items={items}
+                IconRenderer={Icon}
+                uniqueKey="id"
+                onSelectedItemsChange={changeSelectedItems}
+                selectedItems={selectedItems}
+                searchPlaceholderText="Search clans..."
+                modalAnimationType="slide"
+                colors={{ primary: "#7C3AED" }}
+                confirmText="Select"
+              />
+            </View>
+          </View>
 
           <FormField
             title="Password"
             value={form.password}
             handleChangeText={(e) => setForm({ ...form, password: e })}
-            otherStyles="mt-7"
+            otherStyles="mt-3"
           />
 
-        <TouchableOpacity className="bg-violet-400 rounded-xl p-4 mt-6 w-[90vw]">
-          <Link href={'/timeline'}>
-          <Text className="text-white font-pregular text-center text-base"> 
-            Log in
-          </Text>
-          </Link>
-        </TouchableOpacity>
+          <TouchableOpacity className="bg-violet-400 rounded-xl p-4 mt-6 w-[90vw]">
+            <Link href={"/timeline"}>
+              <Text className="text-white font-pregular text-center text-base">
+                SignUp
+              </Text>
+            </Link>
+          </TouchableOpacity>
 
           <View className="flex justify-center pt-5 flex-row gap-2">
             <Text className="text-lg text-gray-100 font-pregular">
@@ -78,4 +117,13 @@ const SignIn = () => {
   );
 };
 
-export default SignIn;
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    marginTop: 30,
+    padding: 4,
+  },
+});
+
+export default SignUp;
