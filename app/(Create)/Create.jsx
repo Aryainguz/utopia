24bf0e18ab1b039aeef0ad5e369980c8a00151ba
Aryainguz <aryaninguz369@gmail.com>
2024-06-25 13:53:52 +0530

    feat: Add Create screen and layout component

---
+++ app/(Create)/Create.jsx
@@ -0,0 +1,13 @@
+import { Image, ScrollView, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import CreateCard from '../../components/CreateCard'
+
+const Create = () => {
+  return (
+   <>
+   <CreateCard />
+   </>
+  )
+}
+
+export default Create
--- app/(Create)/_layout.jsx
+++ app/(Create)/_layout.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Stack } from 'expo-router';
-import { TouchableOpacity } from 'react-native';
+import { Text, TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';  // Import the icon library
 import { useRouter } from 'expo-router';
 
@@ -14,12 +14,24 @@ const Layout = () => {
         options={{
           title: 'Create Post',
           headerLeft: () => (
-            <TouchableOpacity onPress={() => router.back()} style={{ marginLeft: 10 }}>
+            <TouchableOpacity onPress={() => router.back()} style={{ marginHorizontal: 10 }}>
               <Icon name="arrow-back" size={25} color="#fff" />
             </TouchableOpacity>
           ),
+             headerRight: () => (
+            <TouchableOpacity
+              onPress={() => {
+                // Logic for posting the tweet can go here
+                console.log('Tweet posted');
+                router.back(); // Navigate back after posting
+              }}
+              style={{ marginRight: 5,backgroundColor: '#7C3AED',marginTop:7,paddingHorizontal: 25,paddingVertical:10,borderRadius:30}}
+            >
+              <Text style={{ color: '#fff', fontSize: 16 }}>Post</Text>
+            </TouchableOpacity>
+          ),
           headerStyle: {
-            backgroundColor: '#7C3AED', 
+            backgroundColor: '#161622', 
           },
           headerTitleStyle: {
             color: '#fff', 
