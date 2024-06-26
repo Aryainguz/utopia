8cfb3dc494a7d99f315564c67e3f75efcb2d68f3
Aryainguz <aryaninguz369@gmail.com>
2024-06-26 09:15:42 +0530

    refactor: Updated navigation in blog card component

--- components/BlogCard.jsx
+++ components/BlogCard.jsx
@@ -26,26 +26,28 @@ const BlogCard = ({ name, time, username, blog, heartCount, uri, id }) => {
   };
   return (
     // using Link asChild to wrap the Pressable component and make it clickable and using Pressable instead of View to make the card clickable since View does not have an onPress prop and using Link asChild passes click functionality to first child
-    <Link href={`/blog/${id}`} asChild>
-      <Pressable className="shadow-lg w-[100vw] p-4 h-auto border-y-[.4px] border-white bg-primary mx-auto">
-        <View className="flex flex-row justify-between items-center mb-2">
-          <View className="flex flex-row items-center">
-            <Image source={{ uri: uri }} className="h-11 w-11 rounded-full" />
-            <Text className="text-lg font-pbold text-left ml-5 bottom-2 text-white">
-              @{name}
-            </Text>
-          </View>
-            <Entypo name="dots-three-vertical"
-             size={22} color="white" onPress={
-              () => {
-                console.log("clicked");
-              }
-            } />
+    <View className="shadow-lg w-[100vw] p-4 h-auto border-y-[.4px] border-white bg-primary mx-auto">
+      <View className="flex flex-row justify-between items-center mb-2">
+        <View className="flex flex-row items-center">
+          <Image source={{ uri: uri }} className="h-11 w-11 rounded-full" />
+          <Text className="text-lg font-pbold text-left ml-5 bottom-2 text-white">
+            @{name}
+          </Text>
         </View>
-        <Text className="font-pextralight relative left-16 bottom-6 mb-4 text-white">
-          - he/him
-        </Text>
-
+        <Entypo
+          name="dots-three-vertical"
+          size={22}
+          color="white"
+          onPress={() => {
+            console.log("clicked");
+          }}
+        />
+      </View>
+      <Text className="font-pextralight relative left-16 bottom-6 mb-4 text-white">
+        - he/him
+      </Text>
+      
+      <Link href={`/blog/${id}`} asChild>
         <Text className="font-pregular text-md text-left mb-6 text-white">
           {`${blog.slice(0, previewLimit)}`}
           {blog.length > previewLimit ? "..." : ""}
@@ -53,45 +55,41 @@ const BlogCard = ({ name, time, username, blog, heartCount, uri, id }) => {
             {blog.length > previewLimit ? "Show more" : ""}
           </Text>
         </Text>
+      </Link>
 
-        <View
-          style={{
-            position: "absolute",
-            flexDirection: "row",
-            bottom: 13,
-            right: 13,
-            zIndex: 1,
-          }}
-        >
-          <View className="flex flex-row mx-4">
-            <Ionicons name="stats-chart" size={20} color="white" />
-            <Text className="text-sm font-pmedium text-left ml-2 mt-1 text-white">
-              0
-            </Text>
-          </View>
+      <View
+        style={{
+          position: "absolute",
+          flexDirection: "row",
+          bottom: 13,
+          right: 13,
+          zIndex: 1,
+        }}
+      >
+        <View className="flex flex-row mx-4">
+          <Ionicons name="stats-chart" size={20} color="white" />
+          <Text className="text-sm font-pmedium text-left ml-2 mt-1 text-white">
+            0
+          </Text>
+        </View>
 
-          <View className="flex flex-row mr-3">
-            {loved ? (
-              <AntDesign
-                name="heart"
-                size={20}
-                color="red"
-                onPress={handlelog}
-              />
-            ) : (
-              <AntDesign
-                name="hearto"
-                size={20}
-                color="white"
-                onPress={handlelog}
-              />
-            )}
-            <Text className="text-sm font-pmedium text-left relative left-2 top-1 text-white">
-              {showHeartcount}
-            </Text>
-          </View>
+        <View className="flex flex-row mr-3">
+          {loved ? (
+            <AntDesign name="heart" size={20} color="red" onPress={handlelog} />
+          ) : (
+            <AntDesign
+              name="hearto"
+              size={20}
+              color="white"
+              onPress={handlelog}
+            />
+          )}
+          <Text className="text-sm font-pmedium text-left relative left-2 top-1 text-white">
+            {showHeartcount}
+          </Text>
+        </View>
 
-          {/* {
+        {/* {
         loved ? <AntDesign name="heart" size={20} color="red"
         onPress={handlelog}
         /> :  
@@ -103,13 +101,12 @@ const BlogCard = ({ name, time, username, blog, heartCount, uri, id }) => {
         <Text className="text-sm font-pmedium text-left relative left-1 mt-2 text-white">{showHeartcount}</Text>
 
         </View> */}
-        </View>
+      </View>
 
-        <Text className="text-sm font-pextralight text-left mt-2 text-white">
-          {time}
-        </Text>
-      </Pressable>
-    </Link>
+      <Text className="text-sm font-pextralight text-left mt-2 text-white">
+        {time}
+      </Text>
+    </View>
   );
 };
 
